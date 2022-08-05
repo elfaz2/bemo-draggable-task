91a590ca42d92f083792786a3b70444bf3413227
Elfaz Aklilu <elfaz.aklilu2@gmail.com>
2022-08-05 03:57:54 -0400

    Home component updated

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-      Home
-  </div>
+    <div class="home_section">
+        <Header/>
+        <Column/>
+        <Column/>
+        <div class="add_column_container">
+            Add Column
+        </div>
+    </div>
 </template>
 
 <script>
+import Column from "../components/Column";
+import draggable from "vuedraggable";
+import Header from "../components/Header";
 export default {
-    name: "Home"
+    name: "Home",
+    components: {
+        Header,
+        Column,
+        draggable
+    },
+    data() {
+        return {
+        };
+    },
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.home_section {
+    display: flex;
+    gap: 20px;
+    overflow-x: scroll;
+    .add_column_container {
+        margin-top: 120px;
+        background-color: rgba(255, 255, 255, .7);
+        border-radius: 10px;
+        width: 200px;
+        height: 80px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border: 1px dashed black;
+        cursor: pointer;
+    }
+}
 </style>
