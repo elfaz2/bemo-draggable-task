ab96bf7206ee20eb492ed0fb6db4d384e48ef9ac
Elfaz Aklilu <elfaz.aklilu2@gmail.com>
2022-08-05 04:26:11 -0400

    route added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Draggable\CardController;
+use App\Http\Controllers\Draggable\ColumnController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([],function () {
+    Route::get('/columns', [ColumnController::class, 'index']);
+    Route::post('/column', [ColumnController::class, 'store']);
 });
