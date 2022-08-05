fc2614e68fc226e96d90ceebc6d1b19d109e1ac9
Elfaz Aklilu <elfaz.aklilu2@gmail.com>
2022-08-05 05:05:21 -0400

    register modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 import './bootstrap';
 import Vue from 'vue';
+import VModal from 'vue-js-modal'
+import 'vue-js-modal/dist/styles.css'
+
 
 Vue.component('home', require('./pages/Home').default);
+Vue.use(VModal, {})
 
 const app = new Vue({
     el: '#app',
