8cf56e9d7737595f9f2de856e1f6896bba5648a5
Elfaz Aklilu <elfaz.aklilu2@gmail.com>
2022-08-05 08:20:53 -0400

    detail card view implemented

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,7 +1,28 @@
 <template>
-    <div class="card_container">
-        {{ card.title }}
-    </div>
+  <div>
+      <div class="card_container"  v-on:click="$modal.show('detail-card')">
+          {{ card.title }}
+      </div>
+      <Modal class="detail-modal" name="detail-card">
+          <div class="detail_card_section">
+              <div class="header_section">
+                  <label>Card Detail</label>
+                  <button  @click="$modal.hide('detail-card')">X</button>
+              </div>
+              <div class="detail_section">
+                  <div>
+                      Title:
+                       <h3> {{ card.title }} </h3>
+                  </div>
+                  <br>
+                  <div>
+                      Description:
+                      <p> {{card.description }}</p>
+                  </div>
+              </div>
+          </div>
+      </Modal>
+  </div>
 </template>
 
 <script>
@@ -17,6 +38,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.detail-modal {
+    .vm--modal {
+        width: 50% !important;
+        height: 400px !important;
+    }
+}
 .card_container {
     height: 70px;
     border-radius: 5px;
@@ -24,5 +51,23 @@ export default {
     margin-bottom: 14px;
     cursor: grabbing;
     background-color: rgba(255, 255, 255, .7);
+}
+.detail_card_section {
+
+    .header_section {
+        background-color: rgba(0, 0, 0, .3);
+        height: 50px;
+        padding: 10px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .detail_section {
+        padding: 20px;
+        p, h3 {
+            margin-top: 2px;
+            margin-left: 20px;
+        }
+    }
 }
 </style>
